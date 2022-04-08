5e9e67d5dbb626cd1ec9100199252b49c8c1fdca
Rene <renebitter@gmail.com>
2022-04-08 11:36:08 -0300

    Add sessionprovider props in order to optimize http request

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,8 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider>
+    //pageProps.session is provided by profile.js getServerSideProps. Providing it here will skip the "session http request" to check if there is a session when reloading the "/profile". "/auth" on the other hand will send the "session http request" since it does not provide "pageProps.session" hence returning it "undefined"
+    <SessionProvider session={pageProps.session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
